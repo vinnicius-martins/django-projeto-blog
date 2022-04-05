578625044dd548ba546b8ae786c487f5ec22a341
Vinnicius M <vinniciusmartins@dpc.com.br>
2022-04-05 08:29:22 -0300

    commit inicial

---
+++ posts/urls.py
@@ -0,0 +1,10 @@
+import imp
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.PostIndex.as_view(), name='index'),
+    path('categoria/<str:categoria>', views.PostCategoria.as_view(), name='post_categorias'),
+    path('busca/', views.PostBusca.as_view(), name='post_busca'),
+    path('post/<int:pk>', views.PostDetalhes.as_view(), name='post_detalhes'),
+]
