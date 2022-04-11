11c4cb84af953cc7fb9694d3d7763e4ff78aae64
eh o vinni home offices <vinniciusmartins@dpc.com.br>
2022-04-10 21:31:26 -0300

    atualizações

--- blog/settings.py
+++ blog/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -78,12 +79,14 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blog_django',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'blog_django',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '3306',
+        # 'USER': 'root',
+        # 'PASSWORD': 'root',
     }
 }
 
--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,33 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
+from .models import Post
+from django.db.models import Q, Count, Case, When
 
 
 class PostIndex(ListView):
-    pass
+    model = Post
+    template_name = 'posts/index.html'
+    paginate_by = 6
+    context_object_name = 'posts'
+    
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.order_by('-id')
+        qs = qs.filter(publicado_post=True)
+        qs = qs.annotate(
+            numero_comentarios = Count(
+                Case(
+                    When(comentario__publicado_comentario=True, then=1)
+                )
+            )
+        )
+        return qs    
+class PostCategoria(PostIndex):
+    template_name = 'posts/post_categoria.html'
 
 class PostBusca(PostIndex):
-    pass
-
-class PostCategoria(PostIndex):
-    pass
-
+    template_name = 'posts/post_busca.html'
 
 class PostDetalhes(UpdateView):
     pass
